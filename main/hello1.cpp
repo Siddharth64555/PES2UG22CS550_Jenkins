13d1fbeadc7c803c9cd40e7b7dbf1acf72e1d124
Siddharth64555 <siddharthbapupatil@gmail.com>
2025-03-11 23:31:03 +0530

    Create hello1.cpp

---
+++ main/hello1.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout<<"hello PES\n";
+  cout<<"this is for pipeline\n";
+  cout << "I have successfully built and run this  \n";
+  return 0;
+}
